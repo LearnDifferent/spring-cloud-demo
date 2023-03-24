194b7025cca830a0fc56ae59f2683477f8ca7b8f
Zhou <zhou.h.p@outlook.com>
2023-03-24 16:54:12 +0800

    feat: add `springcloud-gateway-9800`

---
+++ springcloud-gateway-9800/src/main/java/com/example/springcloudgateway9800/SpringcloudGateway9800Application.java
@@ -0,0 +1,13 @@
+package com.example.springcloudgateway9800;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudGateway9800Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudGateway9800Application.class, args);
+    }
+
+}
