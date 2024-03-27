70b0411318e1bec6771c41e68bb4a36044b2b828
Zhou <zhou.h.p@outlook.com>
2024-03-27 22:33:39 +0800

    feat(springcloud-alibaba-nacos-config-6300 ConfigDemoController): add `@RefreshScope` annotation

--- springcloud-alibaba-nacos-config-6300/src/main/java/com/example/springcloud/alibaba/controller/ConfigDemoController.java
+++ springcloud-alibaba-nacos-config-6300/src/main/java/com/example/springcloud/alibaba/controller/ConfigDemoController.java
@@ -1,6 +1,7 @@
 package com.example.springcloud.alibaba.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/config")
+@RefreshScope
 public class ConfigDemoController {
 
     /**
